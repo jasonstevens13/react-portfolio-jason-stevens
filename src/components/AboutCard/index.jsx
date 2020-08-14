03e8139aea7b7ce72e3c3e628e0c54ad51b6b110
jasonstevens13 <jasonstevens13@yahoo.com>
2020-08-13 20:15:45 -0400

    bio description and pic updated

--- src/components/AboutCard/index.jsx
+++ src/components/AboutCard/index.jsx
@@ -12,9 +12,7 @@ function AboutCard() {
                     <br></br>
                     <img src="./images/biopic.jpg" className="card-img" alt="Jason Stevens"></img>
                     <p> </p>
-                    <p className="card-text">Flexible, friendly, and self-aware professional with a true desire to build
-                    knowledge and skill set. A loyal employee with a clear objective to make a career
-                            transition into a Junior Developer position that fosters career growth and recognizes existing transferable skills . </p>
+                    <p className="card-text">Jr. Full Stack Developer with a background in roles that fostered skills in management, customer service, digital, and business analysis. Earned a certificate in Full Stack Development through an intensive bootcamp from the University of Pennsylvania (August, 2020). Friendly, well-mannered, team-player, with proven loyal and cooperative interactions with all levels of management, clients, and vendors. Excellent communicator who utilizes good judgment, attention to detail, and passion for research. Quick learner who endures reform and embraces new directives, strategies, and technology.</p>
                 </div>
             </div>
         </div>
