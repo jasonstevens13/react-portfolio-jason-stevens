3c9bfe9c79a94db50333cd59b7865407058f53cf
jasonstevens13 <jasonstevens13@yahoo.com>
2020-08-13 22:15:19 -0400

    tech skills added to about page

--- src/components/AboutCard/index.jsx
+++ src/components/AboutCard/index.jsx
@@ -12,8 +12,18 @@ function AboutCard() {
                     <br></br>
                     <img src="./images/biopic.jpg" className="card-img" alt="Jason Stevens"></img>
                     <p> </p>
-                    <p className="card-text">Jr. Full Stack Developer with a background in roles that fostered skills in management, customer service, digital, and business analysis. Earned a certificate in Full Stack Development through an intensive bootcamp from the University of Pennsylvania (August, 2020). Friendly, well-mannered, team-player, with proven loyal and cooperative interactions with all levels of management, clients, and vendors. Excellent communicator who utilizes good judgment, attention to detail, and passion for research. Quick learner who endures reform and embraces new directives, strategies, and technology.</p>
-                    <p>Email: jasoncstevens13@gmail.com</p>
+                    <div>
+                        <h5>ABOUT:</h5>
+                        <p>Jr. Full Stack Developer with a background in roles that fostered skills in management, customer service, digital, and business analysis. Earned a certificate in Full Stack Development through an intensive bootcamp from the University of Pennsylvania (August, 2020). Friendly, well-mannered, team-player, with proven loyal and cooperative interactions with all levels of management, clients, and vendors. Excellent communicator who utilizes good judgment, attention to detail, and passion for research. Quick learner who endures reform and embraces new directives, strategies, and technology.</p>
+                    </div>
+                    <div>
+                        <h5>TECHNICAL SKILLS:</h5>
+                        <p>MERN Stack | HTML5 | CSS3 | JavaScript | jQuery | Java | Bootstrap | Express.js | React.js | Node.js | Database Theory | MongoDB | MySQL | Command Line | Git | Heroku</p>
+                    </div>
+                    <div>
+                        <h5>EMAIL:</h5>
+                        <p>jasoncstevens13@gmail.com</p>
+                    </div>
                 </div>
             </div>
         </div>
